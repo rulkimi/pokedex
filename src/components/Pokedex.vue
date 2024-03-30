d01e5be773388d0541ac3cc094a97f2493b14880
rulkimi <azrulkimi00@gmail.com>
2024-03-30 18:44:33 +0800

    added color (beta)

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -2,6 +2,7 @@
   <div>
     <div
       class="cursor-pointer inline-flex"
+      :class="[backgroundColor(pokemon.type)]"
       v-for="(pokemon, index) in pokemons" 
       :key="pokemon.name"
       @click="pokemonDetail(index + 1)"
@@ -23,7 +24,74 @@ const pokemons = ref([]);
 
 onMounted(() => {
   getPokemons();
+  console.log(pokemons)
 });
+
+const backgroundColor = pokemonType => {
+  let bgClass = '';
+
+  switch (pokemonType) {
+    case 'normal':
+      bgClass = 'bg-gray-300'; // Normal type background color
+      break;
+    case 'fighting':
+      bgClass = 'bg-red-600'; // Fighting type background color
+      break;
+    case 'flying':
+      bgClass = 'bg-blue-300'; // Flying type background color
+      break;
+    case 'poison':
+      bgClass = 'bg-purple-600'; // Poison type background color
+      break;
+    case 'ground':
+      bgClass = 'bg-yellow-700'; // Ground type background color
+      break;
+    case 'rock':
+      bgClass = 'bg-gray-700'; // Rock type background color
+      break;
+    case 'bug':
+      bgClass = 'bg-green-500'; // Bug type background color
+      break;
+    case 'ghost':
+      bgClass = 'bg-indigo-700'; // Ghost type background color
+      break;
+    case 'steel':
+      bgClass = 'bg-gray-400'; // Steel type background color
+      break;
+    case 'fire':
+      bgClass = 'bg-red-500'; // Fire type background color
+      break;
+    case 'water':
+      bgClass = 'bg-blue-500'; // Water type background color
+      break;
+    case 'grass':
+      bgClass = 'bg-green-500'; // Grass type background color
+      break;
+    case 'electric':
+      bgClass = 'bg-yellow-400'; // Electric type background color
+      break;
+    case 'psychic':
+      bgClass = 'bg-purple-500'; // Psychic type background color
+      break;
+    case 'ice':
+      bgClass = 'bg-blue-200'; // Ice type background color
+      break;
+    case 'dragon':
+      bgClass = 'bg-indigo-500'; // Dragon type background color
+      break;
+    case 'dark':
+      bgClass = 'bg-gray-800'; // Dark type background color
+      break;
+    case 'fairy':
+      bgClass = 'bg-pink-300'; // Fairy type background color
+      break;
+    // Add more cases for other Pokémon types and their corresponding background colors
+    default:
+      bgClass = 'bg-gray-300'; // Default background color
+  }
+
+  return bgClass;
+};
 
 const getPokemons = async () => {
   try {
@@ -43,10 +111,12 @@ const getPokemons = async () => {
         throw new Error('Failed to fetch Pokémon details');
       }
       const pokemonDetails = await pokemonResponse.json();
+      const type = pokemonDetails.types[1]?.type.name === 'flying' ? pokemonDetails.types[1]?.type.name : pokemonDetails.types[0]?.type.name;
       return {
         name: pokemonData.name,
         url: pokemonData.url,
-        image: pokemonDetails.sprites.front_default
+        image: pokemonDetails.sprites.front_default,
+        type: type
       };
     });
 
@@ -71,6 +141,8 @@ const pokemonDetail = async (index) => {
     if (pokemonIndex !== -1) {
       pokemons.value[pokemonIndex].image = responseData.sprites.front_default;
     }
+
+    console.log(responseData)
 
   } catch (error) {
     console.error(error);
