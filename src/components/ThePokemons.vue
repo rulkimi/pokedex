4e15e162bdfe0279f3393bcef3a61e02f58d111d
rulkimi <azrulkimi00@gmail.com>
2024-04-14 18:45:26 +0800

    fix gen 4 not showing

--- src/components/ThePokemons.vue
+++ src/components/ThePokemons.vue
@@ -4,6 +4,7 @@
       <option value="1">Generation 1</option>
       <option value="2">Generation 2</option>
       <option value="3">Generation 3</option>
+      <option value="4">Generation 4</option>
       <option value="5">Generation 5</option>
       <option value="6">Generation 6</option>
       <option value="7">Generation 7</option>
