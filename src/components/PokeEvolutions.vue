a83aa4ce359cbd328c6c3649741e481ecfab8b9b
rulkimi <azrulkimi00@gmail.com>
2024-06-01 22:27:55 +0800

    Revert "handle pokelist responsiveness"
    This reverts commit 315598222e33a6e9ea749009c933c44585ba6abb.

--- src/components/PokeEvolutions.vue
+++ src/components/PokeEvolutions.vue
@@ -2,7 +2,7 @@
   <div class="grid grid-cols-3 gap-4">
     <div v-for="evolution in pokemonEvolutions" :key="evolution.name" class="flex flex-col items-center cursor-pointer" @click="sendPokemonDetail(evolution.name)">
       <img :src="evolution.url" width="100" :alt="'Picture of ' + evolution.name" class="mb-2" />
-      <span class="text-xs sm:text-base md:text-xl">{{ formatName(evolution.name) }}</span>
+      <span>{{ formatName(evolution.name) }}</span>
     </div>
   </div>
 </template>
