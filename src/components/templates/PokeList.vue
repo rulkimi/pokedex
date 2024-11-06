2b7121edf3ac86f35e63cc7ab1c0de9c6510d16d
rulkimi <azrulkimi00@gmail.com>
2024-11-06 22:55:00 +0800

    give some hover effect for pokemon list

--- src/components/templates/PokeList.vue
+++ src/components/templates/PokeList.vue
@@ -22,7 +22,10 @@ defineProps({
 </script>
 
 <template>
-  <li class="border border-gray-300 rounded-xl px-5 py-2 md:py-0 mb-2 cursor-pointer hover:bg-gray-100 flex items-center justify-between md:w-[300px]">
+  <li
+    :class="`hover:bg-${getTypeColor(types[0])}/20 hover:!border-${getTypeColor(types[0])}`"
+    class="group border border-gray-300  rounded-xl px-5 py-2 md:py-0 mb-2 cursor-pointer transition-all duration-500 flex items-center justify-between md:w-[300px]"
+  >
     <div class="flex flex-col mr-4">
       <div class="flex items-center">
         <div class="text-sm text-gray-500 mr-2">{{ '#' + formatIndex(index) }}</div>
@@ -32,7 +35,7 @@ defineProps({
       <div class="flex flex-wrap mt-2">
         <div
           v-for="pokemonType in types"
-          :class="[getTypeColor(pokemonType)]"
+          :class="`bg-${getTypeColor(pokemonType)}`"
           class="rounded-full text-white text-xs px-2 py-1 pb-1.5 mr-1 mb-1"
         >
           {{ pokemonType }}
@@ -40,6 +43,6 @@ defineProps({
       </div>
     </div>
     
-    <img :src="picture" :alt="'Picture of ' + name" class="!w-20 !h-20 object-contain md:w-full">
+    <img :src="picture" :alt="'Picture of ' + name" class="!w-20 !h-20 object-contain md:w-full group-hover:scale-150 group-hover:rotate-6 transition-transform duration-500">
   </li>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,25 @@ export default {
     extend: {},
   },
   plugins: [],
-}
-
+  safelist: [
+    // Add the type colors with background, transparency, hover, and border variants
+    'bg-emerald-500', 'hover:bg-emerald-500/20', 'hover:!border-emerald-500',
+    'bg-red-700', 'hover:bg-red-700/20', 'hover:!border-red-700',
+    'bg-blue-500', 'hover:bg-blue-500/20', 'hover:!border-blue-500',
+    'bg-yellow-400', 'hover:bg-yellow-400/20', 'hover:!border-yellow-400',
+    'bg-green-700', 'hover:bg-green-700/20', 'hover:!border-green-700',
+    'bg-fuchsia-800', 'hover:bg-fuchsia-800/20', 'hover:!border-fuchsia-800',
+    'bg-gray-400', 'hover:bg-gray-400/20', 'hover:!border-gray-400',
+    'bg-cyan-300', 'hover:bg-cyan-300/20', 'hover:!border-cyan-300',
+    'bg-yellow-900', 'hover:bg-yellow-900/20', 'hover:!border-yellow-900',
+    'bg-indigo-400', 'hover:bg-indigo-400/20', 'hover:!border-indigo-400',
+    'bg-pink-500', 'hover:bg-pink-500/20', 'hover:!border-pink-500',
+    'bg-yellow-600', 'hover:bg-yellow-600/20', 'hover:!border-yellow-600',
+    'bg-indigo-700', 'hover:bg-indigo-700/20', 'hover:!border-indigo-700',
+    'bg-indigo-900', 'hover:bg-indigo-900/20', 'hover:!border-indigo-900',
+    'bg-gray-800', 'hover:bg-gray-800/20', 'hover:!border-gray-800',
+    'bg-gray-600', 'hover:bg-gray-600/20', 'hover:!border-gray-600',
+    'bg-pink-300', 'hover:bg-pink-300/20', 'hover:!border-pink-300',
+    'bg-gray-500', 'hover:bg-gray-500/20', 'hover:!border-gray-500',
+  ],
+};
