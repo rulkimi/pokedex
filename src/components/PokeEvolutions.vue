7dbae08d1629523e596dd50917e0d94469ff48a4
rulkimi <azrulkimi00@gmail.com>
2024-11-06 21:09:15 +0800

    cleanup pokeevolutions

--- src/components/PokeEvolutions.vue
+++ src/components/PokeEvolutions.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { formatName } from '../utils/formatHelper';
+import { formatName } from '@/utils/formatHelper';
 import { defineProps, defineEmits } from 'vue';
+
 import axios from 'axios';
 
 defineProps({
@@ -15,22 +16,29 @@ const emit  = defineEmits(['pokemonDetail']);
 const sendPokemonDetail = async (pokemonName) => {
   try {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    console.log(response)
     const { data } = response;
-    console.log(data)
 
     emit('pokemonDetail', data);
-    
   } catch (error) {
     console.error('Error fetching pokemon detail', error);
   }
-}
+};
 </script>
 
 <template>
   <div class="grid grid-cols-3 gap-4">
-    <div v-for="evolution in pokemonEvolutions" :key="evolution.name" class="flex flex-col items-center cursor-pointer" @click="sendPokemonDetail(evolution.name)">
-      <img :src="evolution.url" width="100" :alt="'Picture of ' + evolution.name" class="mb-2" />
+    <div 
+      v-for="evolution in pokemonEvolutions" 
+      :key="evolution.name" 
+      class="flex flex-col items-center cursor-pointer" 
+      @click="sendPokemonDetail(evolution.name)"
+    >
+      <img 
+        :src="evolution.url" 
+        width="100" 
+        :alt="'Picture of ' + evolution.name" 
+        class="mb-2"
+      />
       <span>{{ formatName(evolution.name) }}</span>
     </div>
   </div>
