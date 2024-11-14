868964f38504d9568f5f7c4447de66fbf983152f
rulkimi <azrulkimi00@gmail.com>
2024-11-15 00:02:53 +0800

    change stats design

--- src/components/templates/PokemonDetail.vue
+++ src/components/templates/PokemonDetail.vue
@@ -28,19 +28,17 @@ const goBack = () => {
   </div>
 
   <h2 class="text-lg md:text-xl text-start font-bold mb-2">Base Stats</h2>
-  <div v-for="stat in pokemonDetail.stats" :key="stat.stat.name" class="flex items-center">
+  <div v-for="stat in pokemonDetail.stats" :key="stat.stat.name" class="flex items-center -mr-2 font-semibold">
     <div class="flex-none text-start md:w-1/6 sm:w-1/5 text-xs md:text-sm" style="width: 50px;">{{ formatStat(stat.stat.name) }}</div>
     <div class="flex-grow bg-gray-200 rounded-full h-3 md:h-4">
-      <div class="flex justify-center bg-blue-600 h-3 md:h-4 rounded-full text-white text-xxs md:text-xs" :style="{ width: getStatWidth(stat) + '%' }">
-        <span class="flex items-center">{{ stat.base_stat + ' / ' + getMaxStat(stat) }}</span>
-      </div>
+      <div class="flex justify-center bg-blue-600 h-3 md:h-4 rounded-full text-white text-xxs md:text-xs" :style="{ width: getStatWidth(stat) + '%' }"></div>
     </div>
+    <div class="w-12 flex justify-center">{{ stat.base_stat }}</div>
   </div>
 
-
-  <div class="flex items-center">
-    <div class="flex items-center text-xs md:text-sm">TOTAL STATS:</div>
-    <div class="flex items-center bg-blue-600 h-3 md:h-4 px-2 ml-2 rounded-full text-white text-xxs md:text-xs">{{ getTotalStats(pokemonDetail.stats) }}</div>
+  <div class="flex items-center justify-end mt-2 -mr-1 gap-4">
+    <span class="font-semibold">TOTAL STATS: </span>
+    <span class="px-2 py-1 rounded-full bg-blue-600 text-white ">{{ getTotalStats(pokemonDetail.stats) }}</span>
   </div>
 </template>
 
