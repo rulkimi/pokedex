15ef04eedf5c3fe3df82f483e822499243a09c8a
rulkimi <azrulkimi00@gmail.com>
2024-11-22 08:18:12 +0800

    install fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@nuxtjs/tailwindcss']
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+    '@vesp/nuxt-fontawesome'
+  ]
 })
