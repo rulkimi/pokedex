b047bca6a652f9a4c6d7c364f76f84a57e82d16e
rulkimi <azrulkimi00@gmail.com>
2024-11-23 01:36:10 +0800

    added icon and title

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
-
+useHead({
+  title: 'Pokédex',
+  meta: [
+    { name: 'description', content: 'Explore a detailed collection of Pokémon species, their abilities, evolutions, and more! Dive into the world of Pokémon with comprehensive stats and exciting facts.' }
+  ],
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: '/pokedex-logo.svg'}]
+})
 </script>
 
 <template>
