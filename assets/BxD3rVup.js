343c4b4df665a95446f896fb77f663171bac3fbc
rulkimi <azrulkimi00@gmail.com>
2024-11-24 21:35:52 +0800

    Updates

--- assets/BxD3rVup.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,t as o,v as s,a6 as a}from"./CrmIh43Z.js";const c={},n={class:"container mx-auto p-8 px-4 lg:px-28 xl:px-56"};function r(e,l){return o(),s("div",n,[a(e.$slots,"default")])}const d=t(c,[["render",r]]);export{d as default};
