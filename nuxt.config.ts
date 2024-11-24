ca69b6b1b2d91d61419485ba05738275fd95ecc3
rulkimi <azrulkimi00@gmail.com>
2024-11-24 19:35:25 +0800

    add deployment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/pokedex',
+    buildAssetsDir: 'assets'
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
