44ad9fff60aa93a70206182eab9154a3759cd00d
rulkimi <azrulkimi00@gmail.com>
2025-06-16 20:21:38 +0800

    fix: overflow scroll issue

--- src/app/pokemons/layout.tsx
+++ src/app/pokemons/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <main className="font-mono container space-y-4 mx-auto p-8 px-4 lg:px-27 xl:px-56">
+    <main className="font-mono container mx-auto p-8 px-4 lg:px-27 xl:px-56">
       <DetailsMobileViewProvider>
         <SpriteProvider>
           <TopNav />
