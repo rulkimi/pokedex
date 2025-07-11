4e0f88c4fa6993215852fc6491cf7518017a887b
rulkimi <azrulkimi00@gmail.com>
2025-07-12 07:48:14 +0800

    Revert "fix: add metadata for details"
    This reverts commit 02c65cc43b78af576ba73106044bacd78f1389dc.

--- src/app/pokemons/[gen]/@details/[...pokemonId]/page.tsx
+++ src/app/pokemons/[gen]/@details/[...pokemonId]/page.tsx
@@ -1,7 +1,6 @@
-import { formatName } from "@/lib/utils";
+import { getPokemonImageUrl } from "@/lib/utils";
 import { fetchPokemonById } from "../../actions";
 import PokemonDetail from "./_components/pokemon-detail";
-import { Metadata } from "next";
 
 interface PokemonDetailProps {
   params: Promise<{ pokemonId: number }>
@@ -15,39 +14,6 @@ const PlaceholderText = ({ text }: { text: string}) => {
   );
 };
 
-export async function generateMetadata({ params }: PokemonDetailProps): Promise<Metadata> {
-  const { pokemonId } = await params;
-  
-  if (pokemonId === 0) {
-    return {
-      title: 'Select a Pokemon'
-    }
-  }
-
-  const pokemon = await fetchPokemonById(pokemonId);
-
-  if (!pokemon) {
-    return {
-      title: 'Pokemon Not Found'
-    }
-  }
-
-  return {
-    title: `${formatName(pokemon.name)} - Pokémon Details`,
-    description: `View details about ${pokemon.name}, Pokémon #${pokemon.id}`,
-    openGraph: {
-      images: [
-        {
-          url: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`,
-          width: 475,
-          height: 475,
-          alt: pokemon.name
-        }
-      ]
-    }
-  }
-}
-
 export default async function PokemonDetailPage({ params }: PokemonDetailProps) {
   const { pokemonId } = await params;
 
@@ -59,3 +25,4 @@ export default async function PokemonDetailPage({ params }: PokemonDetailProps) 
 
   return <PokemonDetail pokemon={pokemon} />
 }
+
